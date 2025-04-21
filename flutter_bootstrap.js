75170a3dbb9a72995afea12cfcfa2da1aefeeb0c
NukeMinecart <NukeMinecart@users.noreply.github.com>
2025-04-21 13:50:46 +0000

    deploy: 420c652f1c3a3dd653b3cb4d98d40790e56f6571

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2263796721"
+    serviceWorkerVersion: "1452135478"
   }
 });
