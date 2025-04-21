608aacb1f85e340fe6f70fd39f4add788b7c727b
sharmnten <sharmnten@gmail.com>
2025-04-21 09:58:28 -0500

    Appwrite integration!

--- lib/services/appwrite.dart
+++ lib/services/appwrite.dart
@@ -1,1 +1,12 @@
-void test() => ();
+import 'package:appwrite/appwrite.dart';
+
+/// Returns a configured Appwrite [Client] instance.
+/// Call this function whenever you need the client in your services.
+Client getAppwriteClient() {
+  final client = Client()
+    ..setEndpoint('https://nyc.cloud.appwrite.io/v1')
+    ..setProject('badgerbets2')
+    ..setSelfSigned(status: true); // For self-signed certificates, only use for development
+  return client;
+}
+
