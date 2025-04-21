20a378742040f4428f1e3e7cd68d13b2e2a22de6
sharmnten <sharmnten@gmail.com>
2025-04-21 10:25:11 -0500

    Constructor

--- lib/views/testview.dart
+++ lib/views/testview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TestView extends StatefulWidget {
+  const TestView({super.key});
   @override
   State<TestView> createState() => _TestViewState();
 }
