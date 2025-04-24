9974f1bdc9fe5515b0392766104b6c286aa5de4d
MrAtomicBombHead <MrAtomicBombHead@users.noreply.github.com>
2025-04-24 02:00:09 +0000

    deploy: 5b06d79e0f393e889a483d1b902fd43babb350ec

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3629731867"
+    serviceWorkerVersion: "2499992579"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "canvaskit/skwasm_st.js.symbols": "c7e7aac7cd8b612defd62b43e3050bdd",
 "index.html": "143345954b6afedc13a976b1afb6122b",
 "/": "143345954b6afedc13a976b1afb6122b",
-"flutter_bootstrap.js": "ca4e99a7361f21a56ea468d3fa14968e",
+"flutter_bootstrap.js": "790a40664d2944f8b1dcae46b3bc37de",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
